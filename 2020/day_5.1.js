38db3667fdf2614a0875b42e8f6e3a5770e9a7d5
Miles Evenson <miles.evenson@gmail.com>
2020-12-07 19:33:58 -0500

    2020 - 5.1 - it works

--- 2020/day_5.1.js
+++ 2020/day_5.1.js
@@ -1,4 +1,4 @@
-const seats = require('./day_5.data');
+const data = require('./day_5.data');
 
 
 const ROW_MAX = 127;
@@ -8,45 +8,68 @@ const ROW_BLOCK_LENGTH = 7;
 const COL_BLOCK_LENGTH = 3;
 
 
-function stringToRow(steps) {
-  let c = 0;
-  // TODO: do min/max here instead of a single row
-  let row = ROW_MAX;
-  let direction = 0;
-  while (c < ROW_BLOCK_LENGTH) {
-    if (steps[c] === 'F') {
-      direction = -1;
-    } else if (steps[c] === 'B') {
-      direction = 1;
+function stringToRow(data) {
+  let front = 0;
+  let back = ROW_MAX;
+  let step = 0;
+  let i = 0;
+  while (i < ROW_BLOCK_LENGTH) {
+    step = (ROW_MAX + 1) / Math.pow(2, (i + 1));
+    if (data[i] === 'F') {
+      back -= step;
+    } else if (data[i] === 'B') {
+      front += step;
     }
     // else - zomgwtf
-    row = row + (direction * ((ROW_MAX+1) / Math.pow(2, (c+1))));
-    console.log(`step ${steps[c]} at ${c} means ${(direction * ((ROW_MAX+1) / Math.pow(2, (c+1))))}`);
-    c++;
+    i++;
   }
-  return row;
+  return front;
 }
 
-function stringtoColumn(rawSteps) {
-  let c = ROW_BLOCK_LENGTH;
-  while (c < (ROW_BLOCK_LENGTH + COL_BLOCK_LENGTH)) {
-    c++;
+function stringtoColumn(data) {
+  let left = 0;
+  let right = COL_MAX;
+  let step = 0;
+  let i = 0;
+  let OFFSET = ROW_BLOCK_LENGTH;
+  while (i < COL_BLOCK_LENGTH) {
+    step = (COL_MAX + 1) / Math.pow(2, (i + 1));
+    if (data[(i + OFFSET)] === 'L') {
+      right -= step;
+    } else if (data[(i + OFFSET)] === 'R') {
+      left += step;
+    }
+    // else - zomgwtf
+    i++;
   }
-  return 0;
+  return left;
 }
 
 
-let max = 0;
-let row = 0;
-let col = 0;
-let s = 0;
-let c = 0;
-while (s < seats.length) {
-  row = stringToRow(seats[s]);
-  //col = stringToRow(seats[s]);
-  if (10 <= s) { break; }
-  s++;
+function calculateId(row, column) {
+  return (row * 8) + column;
 }
 
+
+function findMaxId(tickets) {
+  let id = 0;
+  let max = 0;
+  let row = 0;
+  let col = 0;
+  let t = 0;
+  while (t < tickets.length) {
+    row = stringToRow(tickets[t]);
+    col = stringtoColumn(tickets[t]);
+    id = calculateId(row, col);
+    if (max < id) {
+      max = id;
+    }
+    t++;
+  }
+  return max;
+}
+
+const maxId = findMaxId(data);
+console.log(`Max Seat ID: ${maxId}`);
 
 
