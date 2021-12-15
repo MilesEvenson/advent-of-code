33f5c0a4bb865fe6f7b35dbd7677a15ecdf9bbc6
Miles Evenson <miles.evenson@gmail.com>
2021-12-15 12:11:51 -0500

    [2021] - Day15.1 - Initial Greedy Algo

--- 2021/lib/day_15_1.ts
+++ 2021/lib/day_15_1.ts
@@ -36,6 +36,12 @@ const SAMPLE_GRID = [
   '2311944581',
 ];
 
+const SAMPLE_SMALL = [
+  '116',
+  '138',
+  '213',
+];
+
 const SAMPLE_TWISTER = [
   '1169751742',
   '1381373672',
@@ -50,9 +56,30 @@ const SAMPLE_TWISTER = [
 ];
 
 
+const C_RESET = '\x1b[0m';
+
+
+interface Step {
+  row: number;
+  col: number;
+  risk: number;
+  pathRisk: number;
+}
+
+
+interface Coord {
+  row: number;
+  col: number;
+}
+
+
 function getGrid(): string[] {
   let rawLines = SAMPLE_GRID;
-  if (process.env.SOURCE === 'FULL') {
+  if (process.env.SOURCE === 'SMALL') {
+    rawLines = SAMPLE_SMALL;
+  } else if (process.env.SOURCE === 'TWISTER') {
+    rawLines = SAMPLE_TWISTER;
+  } else if (process.env.SOURCE === 'FULL') {
     const buffInput = fs.readFileSync(path.join(__dirname, 'data', 'day_15.input.txt'));
     rawLines = buffInput.toString()
       .trim()
@@ -72,6 +99,149 @@ function parseGrid(rawLines: string[]): number[][] {
     return row;
   });
 }
+
+
+function calculateAverages(grid: number[][], offset: number): number[][] {
+  const averages: number[][] = [];
+  let row: number[];
+  let rowMin = 0;
+  let rowMax = 0;
+  let colMin = 0;
+  let colMax = 0;
+  let areaSum = 0;
+  let areaSize = 0;
+  let average = 0.0;
+
+  for (let r = 0; r < grid.length; r++) {
+    row = [];
+    rowMin = Math.max(0, r-offset);
+    rowMax = Math.min(grid.length-1, r+offset);
+    for (let c = 0; c < grid[r].length; c++) {
+      colMin = Math.max(0, c-offset);
+      colMax = Math.min(grid[r].length-1, c+offset);
+      areaSize = 0;
+      areaSum = 0;
+
+      //for (let y = rowMin; y <= rowMax; y++) {
+      //  for (let x = colMin; x <= colMax; x++) {
+      //    areaSize++;
+      //    areaSum += grid[y][x];
+      //  }
+      //}
+      if (0 < r) {
+        areaSize++;
+        areaSum += grid[r-1][c];
+      }
+      if (c < grid[r].length-1) {
+        areaSize++;
+        areaSum += grid[r][c+1];
+      }
+      if (r < grid.length-1) {
+        areaSize++;
+        areaSum += grid[r+1][c];
+      }
+      if (0 < c) {
+        areaSize++;
+        areaSum += grid[r][c-1];
+      }
+
+      average = Math.floor(100 * (areaSum / areaSize)) / 100;
+      row.push(average);
+    }
+    averages.push(row);
+  }
+
+  return averages;
+}
+
+
+function printHeatmap(grid: number[][]): void {
+
+  // 9 colors to make it easy to map value -> color
+  const spectrum = [
+    // black
+    //'\x1b[2m\x1b[30m',
+    //'\x1b[1m\x1b[30m',
+    //'\x1b[30m',
+
+    // red
+    //'\x1b[2m\x1b[31m',
+    '\x1b[1m\x1b[31m',
+    '\x1b[31m',
+
+    // yellow
+    //'\x1b[2m\x1b[33m',
+    '\x1b[33m',
+    //'\x1b[1m\x1b[33m',
+
+    // green
+    //'\x1b[2m\x1b[32m',
+    '\x1b[32m',
+    //'\x1b[1m\x1b[32m',
+
+    // blue
+    //'\x1b[2m\x1b[34m',
+    '\x1b[34m',
+    //'\x1b[1m\x1b[34m',
+
+    // cyan
+    //'\x1b[2m\x1b[36m',
+    '\x1b[36m',
+    //'\x1b[1m\x1b[36m',
+
+    // white
+    //'\x1b[2m\x1b[37m',
+    '\x1b[37m',
+    '\x1b[1m\x1b[37m',
+
+    // bg white / fg black
+    '\x1b[47m\x1b[30m',
+
+  ];
+  console.log(spectrum.map(c => `${c}0${C_RESET}`).join(''));
+
+  let rowVals: string[] = [];
+  let colorIndex = 0;
+  let color = '';
+  for (let r = 0; r < grid.length; r++) {
+    rowVals = [];
+    for (let c = 0; c < grid[r].length; c++) {
+      colorIndex = Math.round(grid[r][c]) - 1;
+      color = spectrum[colorIndex];
+      rowVals.push(`${color}${Math.round(grid[r][c])}${C_RESET}`);
+    }
+    console.log(rowVals.join(''));
+  }
+}
+
+
+function printPath(
+  steps: Step[],
+  rowMax: number,
+  colMax: number
+): void {
+  let line: string[];
+  let cell: string;
+  for (let r = 0; r <= rowMax; r++) {
+    line = [];
+    for (let c = 0; c <= colMax; c++) {
+      cell = ' ';
+      steps.forEach((s, idx) => {
+        if (s.row == r && s.col == c) {
+          cell = `${idx}`;
+        }
+      });
+      line.push(cell);
+    }
+    console.log(line.join(''));
+  }
+}
+
+
+function getSlug(row: number, col: number): string {
+  return `${row}-${col}`;
+}
+
 
 
 function day15_1(): void {
@@ -140,11 +310,166 @@ function day15_1(): void {
   //
   //
   // Hmmm
-  // 
-  // 
+  // I'll code up an exploratory function to calculate
+  // the average safety for +2/-2 around each cell.
+  // Using a radius of 2 wasn't helpful. Most cells average to 4.
+  //
+  // Using a radius of 1 was a little more helpful, but didn't
+  // provide a clear path. There are many blocks of 4 and 5 averages.
+  //
+  //
+  // My gut wants me to re-consider Divide and Conquer.
+  //  1. How do I select the initial endpoints?
+  //  2. How determine the path between? Brute force at the base case?
+  //  2. How do I adjust the endpoints?
+  //
+  // What is base case? 4x4?
+  // I'm fine with hard-coding that for this problem.
+  // Probably choose max(2, lowest divisor) if input varies.
+  // Prefer 3x3 or 4x4 or 5x5 to 2x2.
+  //
+  // For given [row, col]
+  //  - if base
+  //    - return brute force path
+  //  - find diagonal mid,
+  //  - get NW path
+  //  - get SE path
+  //  - return NW + SE path
+  //
+  // Still not sure how to adjust endpoints.
+  // Finding optimal paths for [0,0] - [4,4]
+  // and [4, 4] - [9,9] does not get me close
+  // to the global minimum path.
+  //
+  //
+  // Fine.
+  // What does a modified Greedy Algo look like?
+  //  1. Establish base path with Greedy Algorithm.
+  //  2. Working backwards step-by-step,
+  //      brute force alternate paths until,
+  //        - incomplete alt path has higher risk than base path
+  //        - complete alt path is safer than base path
+  //  3. If alt path is better,
+  //      continue exploring alt paths from the branch point.
+  //
+  // Ignoring lookahead for now to avoid additional complexity.
+  //
+  // Seems fine at a high level.
+  // What does implementation look like?
+  //  - basePath <= Greedy Algo
+  //  - stepping backwards through basePath,
+  //    - recursive brute force alternates.
+  //    - if altPath < basePath
+  //      - update basePath for record keeping
+  //    - continue stepping backwards through path.
+  //
+  // My Greedy Algo:
+  //  for a given cell, move to safest unvisited neighbor
+  //    if tie, pick South-er, or East-er branch.
+  //
+
+
+  function getOpenNeighborCoords(
+    row: number,
+    col: number,
+    rowMax: number,
+    colMax: number
+  ): Coord[] {
+    let coords: Coord[] = [];
+    const slugNorth = getSlug(row-1, col);
+    const slugEast = getSlug(row, col+1);
+    const slugSouth = getSlug(row+1, col);
+    const slugWest = getSlug(row, col-1);
+
+    if (0 < row && !visitedCells[0].hasOwnProperty(slugNorth)) {
+      coords.push({ row: row-1, col: col });
+    }
+    if (col < colMax && !visitedCells[0].hasOwnProperty(slugEast)) {
+      coords.push({ row: row, col: col+1 });
+    }
+    if (row < rowMax && !visitedCells[0].hasOwnProperty(slugSouth)) {
+      coords.push({ row: row+1, col: col });
+    }
+    if (0 < col && !visitedCells[0].hasOwnProperty(slugWest)) {
+      coords.push({ row: row, col: col-1 });
+    }
+    return coords;
+  }
 
 
   const grid = parseGrid(getGrid());
+  //printHeatmap(calculateAverages(grid, 1));
+  const rowMax = grid.length - 1;
+  const colMax = grid[0].length - 1;
+
+  const visitedCells: Record<string, number>[] = [{'0-0': 0}];
+  const allPaths: Step[][] = [];
+  let basePath: Step[] = [
+    {
+      row: 0,
+      col: 0,
+      risk: 0,
+      pathRisk: 0,
+    },
+  ];
+  let tip = 0;
+  let allNeighbors: Coord[] = [];
+  let openNeighbors: Coord[];
+  let minRiskCoord: Coord;
+
+
+  function moveGreedy(row: number, col: number, pathRisk: number): Step[] {
+    const openNeighbors = getOpenNeighborCoords(row, col, rowMax, colMax);
+    console.log(`  cell (${getSlug(row, col)} ${grid[row][col]}) has ${openNeighbors.length} open neighbors.`);
+    console.log(`    ${openNeighbors.map(n => `(${getSlug(n.row, n.col)} ${grid[n.row][n.col]})`)}`);
+    if (openNeighbors.length == 0) {
+      return [];
+    }
+
+    visitedCells[0][getSlug(row, col)] = grid[row][col];
+
+    openNeighbors.sort((a, b) => (grid[a.row][a.col] - grid[b.row][b.col]));
+    let subPath: Step[];
+    for (const neb of openNeighbors) {
+      // Finish if the current neighbor is the exit.
+      if (neb.row == rowMax && neb.col == colMax) {
+        console.log('  *** found the exit ***');
+        return [
+          {
+            row: row,
+            col: col,
+            risk: grid[row][col],
+            pathRisk: (pathRisk + grid[row][col]),
+          },
+          {
+            row: neb.row,
+            col: neb.col,
+            risk: grid[neb.row][neb.col],
+            pathRisk: (pathRisk + grid[row][col] + grid[neb.row][neb.col]),
+          }
+        ];
+      }
+      subPath = moveGreedy(neb.row, neb.col, (pathRisk + grid[row][col]));
+      if (0 < subPath.length) {
+        subPath.unshift({
+          row: row,
+          col: col,
+          risk: grid[row][col],
+          pathRisk: (pathRisk + grid[row][col]),
+        });
+        return subPath;
+      }
+      console.log(`  subPath to (${getSlug(neb.row, neb.col)} ${grid[neb.row][neb.col]}) did not work`);
+    }
+
+    delete visitedCells[0][getSlug(row, col)];
+    return [];
+  }
+
+  allPaths.push(moveGreedy(0, 0, 0));
+  console.dir(allPaths);
+  printPath(allPaths[0], rowMax, colMax);
+
 
   const pathLength = 0;
   const totalRiskLevel = 0
