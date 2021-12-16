1076d76b49fa5b548b3a99addbe43e78f4e897cb
Miles Evenson <miles.evenson@gmail.com>
2021-12-16 12:20:37 -0500

    [2021] - Day 16.1 - Working solution
    Relies on logging out the nested packet
    structure instead of processing it at
    runtime to determine all the version
    numbers and sum them.

--- 2021/lib/day_16_1.ts
+++ 2021/lib/day_16_1.ts
@@ -33,20 +33,23 @@ import path from 'path';
 //  a start index as a param
 //
 //
-//
-//
-//
 // Complexity Analysis:
+//  B: length of binary string
+//  P: number of packets
 //
 //  Time Complexity:
+//    O(B)
+//    Process each binary digit once
+//    Ideally this is O(B+P) to include a recusive
+//    traversal of the nested packet structure to
+//    compile a list of all version numbers,
+//    but I couldn't quite gt that to work.
 //
 //  Space complexity:
+//    O(B + P)
+//    Store complete binary string in memory.
+//    Store each packet in memory.
 //
-//
-// Total time,    ????  minutes
-//  Planning       ??
-//  Programming    ??
-//  Debugging      ??
 //
 //
 //
@@ -68,14 +71,27 @@ interface Example {
 
 
 interface Packet {
+  lengthType?: LengthType,
+  offset: number;
   rawData: string;
+  subPacketBitLength: number;
   subPackets: Packet[];
   typeID: number;
+  totalLength: number;
   version: number;
 }
 
 
 const TYPE_LITERAL = 4;
+
+
+enum LengthType {
+  TotalBitLength = '0',
+  CountSubPackets = '1',
+}
+
+
+const C_RESET = '\x1b[0m';
 
 
 const SAMPLE_DATA: Record<string, Example> = {
@@ -164,71 +180,185 @@ function hex2bin(hex: string): string {
 }
 
 
+function dec2binString(int: number): string {
+  return `${Number(int).toString(2)}`;
+}
+
+
+function padBinString(str: string, targetLength: number): string {
+  while (str.length < targetLength) {
+    str = '0' + str;
+  }
+  return str;
+}
+
+
+function packetToBinString(packet: Packet): string {
+  const chunks: string[] = [];
+  const binVersion = padBinString(dec2binString(packet.version), 3);
+  const binTypeID = padBinString(dec2binString(packet.typeID), 3);
+  chunks.push(`\x1b[32m${binVersion}${C_RESET}`);
+  chunks.push(`\x1b[33m${binTypeID}${C_RESET}`);
+  if (packet.typeID == 4) {
+    chunks.push(`\x1b[40m\x1b[37m${packet.rawData}${C_RESET}`);
+  } else {
+    chunks.push(`\x1b[36m${packet.lengthType}${C_RESET}`);
+    if (packet.lengthType == LengthType.TotalBitLength) {
+      chunks.push(`\x1b[2m\x1b[31m${padBinString(dec2binString(packet.subPacketBitLength), 15)}${C_RESET}`);
+    } else {
+      chunks.push(`\x1b[33m${padBinString(dec2binString(packet.subPackets.length), 11)}${C_RESET}`);
+    }
+    packet.subPackets.forEach(p => chunks.push(packetToBinString(p)));
+  }
+  return chunks.join('');
+}
+
+
 function processTransmission(hexData: string): Packet {
   const binData = hex2bin(hexData);
-  console.log('Processing transmission:');
-  console.log(hexData);
-  console.log(binData);
+  //console.log('Processing transmission:');
+  //console.log(hexData);
+  //console.log(binData);
+
+  function parseDecInt(idxStart: number, fieldLength: number): number {
+    let cursor = idxStart;
+    while (binData[cursor] == '0' && cursor < (idxStart + fieldLength)) {
+      cursor++;
+    }
+    let bits: string[] = [];
+    for (cursor; cursor < (idxStart + fieldLength); cursor++) {
+      bits.push(binData[cursor]);
+    }
+    if (bits.length == 0) {
+      bits.push('0');
+    }
+    return parseInt(bits.join(''), 2);
+  }
 
   function parseVersion(idxStart: number): number {
-    return parseInt(`${binData[idxStart]}${binData[idxStart+1]}${binData[idxStart+2]}`, 2);
+    return parseDecInt(idxStart, 3);
   }
 
   function parseTypeID(idxStart: number): number {
-    return parseInt(`${binData[idxStart]}${binData[idxStart+1]}${binData[idxStart+2]}`, 2);
+    return parseDecInt(idxStart, 3);
   }
 
-  function parseLiteralValue(idxStart: number): string {
-    // take 5-char words out of binData until finding one that starts with '0'
-    let offset = idxStart;
-    let words: string[] = [];
-    while (binData[offset] != '0') {
-      words.push(`${binData[offset+1]}${binData[offset+2]}${binData[offset+3]}${binData[offset+4]}`);
-      offset += 5;
+  function parseLengthType(idxStart: number): LengthType {
+    if (binData[idxStart] == LengthType.TotalBitLength) {
+      return LengthType.TotalBitLength;
     }
-    words.push(`${binData[offset+1]}${binData[offset+2]}${binData[offset+3]}${binData[offset+4]}`);
+    return LengthType.CountSubPackets;
+  }
+
+  function parseSubPacketBitLength(idxStart: number): number {
+    return parseDecInt(idxStart, 15);
+  }
+
+  function parseSubPacketCount(idxStart: number): number {
+    return parseDecInt(idxStart, 11);
+  }
+
+  function takeRawLiteralValue(idxStart: number): string {
+    //console.log(`    taking raw literal value starting at (${idxStart}).`);
+    // take 5-char words out of binData until finding one that starts with '0'
+    let cursor = idxStart;
+    let words: string[] = [];
+    while (binData[cursor] != '0' && cursor < (binData.length - 10)) {
+      // TODO: make this a little easier to read with a loop?
+      words.push(`${binData[cursor]}${binData[cursor+1]}${binData[cursor+2]}${binData[cursor+3]}${binData[cursor+4]}`);
+      cursor += 5;
+    }
+      // TODO: make this a little easier to read with a loop?
+    words.push(`${binData[cursor]}${binData[cursor+1]}${binData[cursor+2]}${binData[cursor+3]}${binData[cursor+4]}`);
     return words.join('');
   }
 
-  function parsePacket(idxStart: number): Packet {
-    let offset = idxStart;
-    const version = parseVersion(offset);
-    offset += 3;
-    const typeID = parseTypeID(offset);
-    offset += 3;
+  function parseSubPacketsByLength(idxStart: number, totalLength: number): Packet[] {
+    let cursor = idxStart;
+    let subPacket: Packet;
+    const packets: Packet[] = [];
+    while (cursor < (idxStart + totalLength)) {
+      subPacket = parsePacket(cursor);
+      packets.push(subPacket);
+      cursor += subPacket.totalLength;
+    }
+    return packets;
+  }
 
-    if (typeID == TYPE_LITERAL) { 
-      return {
-        rawData: parseLiteralValue(offset),
-        subPackets: [],
-        typeID: typeID,
-        version: version,
-      }
-    } else {
-      // TODO: check length type!
+  function parseSubPacketsByCount(idxStart: number, totalPackets: number): Packet[] {
+    let cursor = idxStart;
+    let subPacket: Packet;
+    const packets: Packet[] = [];
+    while (packets.length < totalPackets) {
+      subPacket = parsePacket(cursor);
+      packets.push(subPacket);
+      cursor += subPacket.totalLength;
     }
 
-    return {
+    return packets;
+  }
+
+  function parsePacket(idxStart: number): Packet {
+    console.log(`parsePacket at (${idxStart})`);
+    let cursor = idxStart;
+    const version = parseVersion(cursor);
+    //console.log(`  parsePacket got version (${version})`);
+    cursor += 3;
+    const typeID = parseTypeID(cursor);
+    //console.log(`  parsePacket got typeID (${typeID})`);
+    cursor += 3;
+
+    const packet: Packet = {
+      offset: idxStart,
       rawData: '',
+      subPacketBitLength: 0,
       subPackets: [],
-      typeID: 0,
-      version: 0,
+      totalLength: 6,
+      typeID: typeID,
+      version: version,
     };
+
+    if (typeID == TYPE_LITERAL) {
+      packet.rawData = takeRawLiteralValue(cursor);
+      packet.totalLength += packet.rawData.length;
+    } else {
+      const lengthType = parseLengthType(cursor);
+      cursor += 1;
+      packet.lengthType = lengthType;
+      packet.totalLength += 1;
+      if (lengthType == LengthType.TotalBitLength) {
+        //console.log(`  new Op packet has length type (${lengthType}), total bit length.`);
+        const totalLength = parseSubPacketBitLength(cursor);
+        cursor += 15;
+        //console.log(`  total bit length is (${totalLength})`);
+        packet.subPackets = parseSubPacketsByLength(cursor, totalLength);
+        packet.subPacketBitLength = totalLength;
+        packet.totalLength += 15;
+      } else {
+        //console.log(`  new Op packet has length type (${lengthType}), count of sub-packets.`);
+        const countSubPackets = parseSubPacketCount(cursor);
+        cursor += 11;
+        //console.log(`  expecting (${countSubPackets}) sub-packets`);
+        packet.subPackets = parseSubPacketsByCount(cursor, countSubPackets);
+        packet.totalLength += 11;
+      }
+      packet.totalLength += packet.subPackets.reduce((s, p) => (s + p.totalLength), 0);
+    }
+
+    //console.log(`Generated packet at offset (${idxStart}):`);
+    //console.log(binData.substring(idxStart, (idxStart + packet.totalLength)));
+    //console.log(packetToBinString(packet));
+    return packet;
   } // end - parsePacket()
 
 
   const rootPacket = parsePacket(0);
-  console.dir(rootPacket);
 
-  return {
-    rawData: '',
-    subPackets: [],
-    typeID: 0,
-    version: 0,
-  };
+  return rootPacket;
 }
 
 
+// TODO: This is flawed in a way that is not immediately obvious to me.
 function analyzePacket(packet: Packet): Analysis {
   const report = {
     totalPackets: 1,
@@ -238,21 +368,34 @@ function analyzePacket(packet: Packet): Analysis {
     return report;
   }
   let subVersions: number[] = [];
+  console.log(`Compiling (${packet.subPackets.length}) subReports`);
   packet.subPackets.forEach(sp => {
     const subReport = analyzePacket(sp);
+    console.dir(subReport, { depth: null, maxArrayLength: null });
     report.totalPackets += subReport.totalPackets;
     subVersions = subVersions.concat(subReport.allVersionNumbers);
+    console.dir(subVersions, { depth: null, maxArrayLength: null });
   });
   report.allVersionNumbers = subVersions;
+  console.dir(report.allVersionNumbers, { depth: null, maxArrayLength: null });
   return report;
 }
 
 
 function processSampleData(): void {
-  Object.values(SAMPLE_DATA)
-    .forEach(example => {
-      processTransmission(example.transmission);
-    });
+  let examplesToProcess: Example[] = Object.values(SAMPLE_DATA);
+
+  if (process.env.LABEL) {
+    examplesToProcess = Object.values(SAMPLE_DATA)
+      .filter(e => e.label == process.env.LABEL);
+  }
+  examplesToProcess.forEach(example => {
+    console.log('--------');
+    console.log(`Processing example transmission ${example.label}`);
+    const packet = processTransmission(example.transmission);
+    console.log('Got packet:');
+    console.dir(packet, { depth: null, maxArrayLength: null });
+  });
 }
 
 
@@ -261,13 +404,22 @@ function day16_1(): void {
   console.log('Welcome to Day 16.1. Packets all the way down.');
 
   if (isFull()) {
-    const fullReport = analyzePacket(processTransmission(getFullTransmission()));
+    const fullPacket = processTransmission(getFullTransmission());
+    console.log('Full input yielded packet:');
+    console.dir(fullPacket, { depth: null, maxArrayLength: null });
+    const fullReport = analyzePacket(fullPacket);
+    //console.log('Analysis of full packet:');
+    //console.dir(fullReport, { depth: null, maxArrayLength: null });
+    console.log(fullReport.allVersionNumbers.join(','));
     const versionsFlat = fullReport.allVersionNumbers.flat(fullReport.totalPackets);
     const sumVersionNumbers = versionsFlat.reduce((s, v) => (s + v), 0);
     console.log(`Processed (${fullReport.totalPackets}) packets to get sum (${sumVersionNumbers}).`);
   } else {
     processSampleData();
   }
+
+  //  630   low
+  // 1938   high
 }
 
 
