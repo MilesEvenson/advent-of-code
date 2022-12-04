856eb260a7df0f9e132f25a58ba61f160270fb03
Miles Evenson <miles.evenson@gmail.com>
2022-12-04 08:55:18 -0500

    [2022] - Day 04.2 - working solution

---
+++ 2022/lib/day_04_2.py
@@ -0,0 +1,53 @@
+import os
+
+from pprint import PrettyPrinter
+
+
+pp = PrettyPrinter(indent=2)
+
+
+def load_assignments():
+    filename = 'day_04.sample.txt'
+    if os.environ.get('TARGET', 'SAMPLE') == 'FULL':
+        filename = 'day_04.full.txt'
+
+    filedir = os.path.dirname(os.path.abspath(__file__))
+    filepath = '{}/../data/{}'.format(filedir, filename)
+
+    raw_lines = ''
+    with open(filepath) as fh:
+        raw_lines = fh.read()
+
+    assignments = []
+    for line in raw_lines.strip().split('\n'):
+        [left, right] = line.split(',')
+        [left_start, left_end] = left.split('-')
+        [right_start, right_end] = right.split('-')
+        assignments.append((
+            (int(left_start), int(left_end)),
+            (int(right_start), int(right_end))))
+
+    return assignments
+
+
+def is_partial_overlap(pair):
+    if pair[0][1] < pair[1][0] or pair[1][1] < pair[0][0]:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    print('Day 4.2 - Any overlap is too much')
+
+    all_assignments = load_assignments()
+    #pp.pprint(all_assignments)
+    #print('\n')
+
+    total_overlaps = 0
+    for pair in all_assignments:
+        if is_partial_overlap(pair):
+            total_overlaps += 1
+
+    print('Total overlaps: {}'.format(total_overlaps))
+
+
