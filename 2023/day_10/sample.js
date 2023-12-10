63871e31c36ceec4f9f5678a83cbe00d5701758e
Miles Evenson <miles.evenson@gmail.com>
2023-12-10 13:00:37 -0500

    2023 - Day 10.1 - solution, but not viz yet

---
+++ 2023/day_10/sample.js
@@ -0,0 +1,21 @@
+function getSimpleSampleData() {
+  return [
+    '-L|F7',
+    '7S-7|',
+    'L|7||',
+    '-L-J|',
+    'L|-JF',
+  ];
+}
+
+
+function getMediumSampleData() {
+  return [
+    '7-F7-',
+    '.FJ|7',
+    'SJLL7',
+    '|F--J',
+    'LJ.LJ',
+  ];
+}
+
