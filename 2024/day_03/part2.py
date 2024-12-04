f5eb6fe7e47e06bb128062eac5424aeb55158ab9
Miles Evenson <miles.evenson@gmail.com>
2024-12-03 20:49:29 -0500

    2024 - Day 3.2 - refine solution

--- 2024/day_03/part2.py
+++ 2024/day_03/part2.py
@@ -25,77 +25,83 @@ def load_data(target):
     return [rl.strip() for rl in raw_lines]
 
 
+def parse_line(line):
+    return pairs
+
+
 def solve(data):
     result = 0
-
-    mode = 'DO'
-
     digits = set([str(n) for n in range(10)])
 
+    # NOTE - mode carries over from one line to the next
+    mode = 'DO'
+
     for line in data:
-        #print('parsing line:\n{}\n'.format(line))
+        # Simple(ish) state machine
+        def next_mode(curr_mode, idx):
+            d = line.find("do()", idx)
+            t = line.find("don't()", idx)
+            m = line.find('mul(', idx)
+
+            if curr_mode == 'DO':
+                if m == -1:
+                    # We're done because there are no more mul(x,y) to capture
+                    return (curr_mode, -1)
+                elif t == -1 or m < t:
+                    # Advance to the next mul(x,y) if there is no don't(),
+                    # or if the next mul(x,y) is closer than the next don't()
+                    return ('DO', m)
+                elif t < m:
+                    # If the next don't() is closer than the next mul(x,y),
+                    # simply switch to DONT mode.
+                    return ('DONT', t)
+                else:
+                    print('ERROR - curr_mode ({}) at idx({}) with d({}), t({}), m({})'.format(
+                        curr_mode, idx, d, t, m))
+                    return (curr_mode, -1)
+
+            elif curr_mode == 'DONT':
+                if d == -1:
+                    # We're done because there are no more do() blocks
+                    return (curr_mode, -1)
+                # If there is another do() block advance to it,
+                # then recurse to advance to the next mul(x,y), if it exists
+                return next_mode('DO', d)
+
+            else:
+                print('ERROR - unknown mode({}) at idx({})'.format(curr_mode, idx))
+                return (curr_mode, -1)
 
         pairs = []
-        d = line.find("don't()")
-        i = line.find('mul(')
-        if d != -1 and d < i:
-            mode = 'DONT'
-            i = d+1
-
+        i = 0
         while i != -1 and i < len(line) - 4:
-            if mode == 'DONT':
-                i = line.find('do()', i)
-                if i == -1:
-                    break
-                #print('found do: {}'.format(line[max(i-3,0):min(i+10,len(line))]))
-                mode = 'DO'
-                i = line.find('mul(', i+1)
-                continue
+            if mode == 'DO':
+                # NOTE - *assumes* that line[i] is the start of an instance of 'mul('
+                left_num_start = i + 4
+                left_num_end = left_num_start
 
-            #print('found instruction start: {}'.format(line[max(i-3,0):min(i+10,len(line))]))
-            int_left = 0
-            int_right = 0
-            num_start = i + 4
-            num_end = num_start
+                # Scan forward for the left number
+                while left_num_end < len(line) and line[left_num_end] in digits:
+                    left_num_end += 1
 
-            while num_end < len(line) and line[num_end] in digits:
-                num_end += 1
-            if line[num_end] != ',':
-                d = line.find("don't()", num_end)
-                i = line.find('mul(', num_end)
-                if d != -1 and d < i:
-                    mode = 'DONT'
-                    i = d+1
-                continue
-            if num_end == len(line):
-                break
-            int_left = int(line[num_start:num_end])
+                right_num_start = left_num_end + 1
+                right_num_end = right_num_start
 
-            num_start = num_end + 1
-            num_end = num_start
+                # Scan forward for the right number
+                while right_num_end < len(line) and line[right_num_end] in digits:
+                    right_num_end += 1
 
-            while num_end < len(line) and line[num_end] in digits:
-                num_end += 1
-            if line[num_end] != ')':
-                d = line.find("don't()", num_end)
-                i = line.find('mul(', num_end)
-                if d != -1 and d < i:
-                    mode = 'DONT'
-                    i = d+1
-                continue
-            if num_end == len(line):
-                break
-            int_right = int(line[num_start:num_end])
+                if left_num_start < left_num_end \
+                    and right_num_start < right_num_end \
+                    and line[right_num_end] == ')':
+                    int_left = int(line[left_num_start:left_num_end])
+                    int_right = int(line[right_num_start:right_num_end])
+                    pairs.append([int_left, int_right])
+                    i = right_num_end
+            
+            # Regardless of mode, or what was parsed, update mode and advance
+            (mode, i) = next_mode(mode, i+1)
 
-            pairs.append([int_left, int_right])
-
-            d = line.find("don't()", num_end)
-            i = line.find('mul(', num_end)
-            if d != -1 and d < i:
-                mode = 'DONT'
-                i = d+1
-
-        #pp.pprint(pairs)
         for ints in pairs:
             result += ints[0] * ints[1]
 
